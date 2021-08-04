685af321ad7141e800968f25ea3d63eea1759a72
vol_dev <voladores@inbox.ru>
2021-08-04 14:31:46 +0300

    ref editor icons

--- src/components/common/Editor.vue
+++ src/components/common/Editor.vue
@@ -8,7 +8,8 @@
         :class="{ 'is-active': editor.isActive(paramsClass) }"
         @click="editor.chain().focus()[onClick](paramsClick).run()"
       >
-        <component :is="icon" class="editor-icon" />
+        <!-- <component :is="icon" class="editor-icon" /> -->
+        <Icon light :type="icon" class="editor-icon" />
       </div>
     </div>
     <EditorContent :editor="editor" />
@@ -16,24 +17,12 @@
 </template>
 
 <script>
-import BtnFormatHeaderOne from '@/assets/editor/format-header-1.svg'
-import BtnFormatHeaderTwo from '@/assets/editor/format-header-2.svg'
-import BtnFormatHeaderTree from '@/assets/editor/format-header-3.svg'
-import BtnFormatParagraph from '@/assets/editor/format-paragraph.svg'
-import BtnFormatBold from '@/assets/editor/format-bold.svg'
-import BtnFormatItalic from '@/assets/editor/format-italic.svg'
-import BtnStrikeThrough from '@/assets/editor/format-strikethrough-variant.svg'
-import BtnHighlight from '@/assets/editor/format-color-highlight.svg'
-import BtnFormatAlignLeft from '@/assets/editor/format-align-left.svg'
-import BtnFormatAlignCenter from '@/assets/editor/format-align-center.svg'
-import BtnFormatAlignRight from '@/assets/editor/format-align-right.svg'
-import BtnFormatAlignJustify from '@/assets/editor/format-align-justify.svg'
-
 import { Editor, EditorContent } from '@tiptap/vue-2'
 import StarterKit from '@tiptap/starter-kit'
 import TextAlign from '@tiptap/extension-text-align'
 import Highlight from '@tiptap/extension-highlight'
 
+import Icon from '@/components/common/Icon.vue'
 
 const BUTTONS = [
   {
@@ -41,97 +30,86 @@ const BUTTONS = [
     paramsClick: { level: 1 },
     onClick: 'toggleHeading',
     text: 'h1',
-    icon: 'BtnFormatHeaderOne',
+    icon: 'h1',
   },
   {
     paramsClass: ('heading', { level: 2 }),
     paramsClick: { level: 2 },
     onClick: 'toggleHeading',
     text: 'h2',
-    icon: 'BtnFormatHeaderTwo',
+    icon: 'h2',
   },
   {
     paramsClass: ('heading', { level: 3 }),
     paramsClick: { level: 3 },
     onClick: 'toggleHeading',
     text: 'h3',
-    icon: 'BtnFormatHeaderTree',
+    icon: 'h3',
   },
   {
     paramsClass: 'paragraph',
     onClick: 'setParagraph',
     text: 'paragraph',
-    icon: 'BtnFormatParagraph',
+    icon: 'paragraph',
   },
   {
     paramsClass: 'bold',
     onClick: 'toggleBold',
     text: 'bold',
-    icon: 'BtnFormatBold',
+    icon: 'bold',
   },
   {
     paramsClass: 'italic',
     onClick: 'toggleItalic',
     text: 'italic',
-    icon: 'BtnFormatItalic',
+    icon: 'italic',
   },
   {
     paramsClass: 'strike',
     onClick: 'toggleStrike',
     text: 'strike',
-    icon: 'BtnStrikeThrough',
+    icon: 'strike',
   },
   {
     paramsClass: 'highlight',
     onClick: 'toggleHighlight',
     text: 'highlight',
-    icon: 'BtnHighlight',
+    icon: 'highlight',
   },
   {
     paramsClass: { textAlign: 'left' },
     paramsClick: 'left',
     onClick: 'setTextAlign',
     text: 'left',
-    icon: 'BtnFormatAlignLeft',
+    icon: 'left',
   },
   {
     paramsClass: { textAlign: 'center' },
     paramsClick: 'center',
     onClick: 'setTextAlign',
     text: 'center',
-    icon: 'BtnFormatAlignCenter',
+    icon: 'center',
   },
   {
     paramsClass: { textAlign: 'right' },
     paramsClick: 'right',
     onClick: 'setTextAlign',
     text: 'right',
-    icon: 'BtnFormatAlignRight',
+    icon: 'right',
   },
   {
     paramsClass: { textAlign: 'justify' },
     paramsClick: 'justify',
     onClick: 'setTextAlign',
     text: 'justify',
-    icon: 'BtnFormatAlignJustify',
+    icon: 'justify',
   },
 ]
 
 
 export default {
   components: {
-    BtnFormatHeaderOne,
-    BtnFormatHeaderTwo,
-    BtnFormatHeaderTree,
-    BtnFormatParagraph,
-    BtnFormatBold,
-    BtnFormatItalic,
-    BtnStrikeThrough,
-    BtnHighlight,
-    BtnFormatAlignLeft,
-    BtnFormatAlignCenter,
-    BtnFormatAlignRight,
-    BtnFormatAlignJustify,
+    Icon,
     EditorContent,
   },
 
