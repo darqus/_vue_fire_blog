07d3de9a8a35e3e5b5a6c62ec3c1de34ee16a294
vol_dev <voladores@inbox.ru>
2021-11-28 06:56:45 +0300

    update sass

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,22 @@ const { version } = require('./package.json')
 
 process.env.VUE_APP_VERSION = version
 
-module.exports = {}
+module.exports = {
+  devServer: {
+    hot: true,
+    overlay: {
+      warnings: true,
+      errors: true,
+    },
+  },
+  lintOnSave: process.env.NODE_ENV !== 'production',
+  /* css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `
+          @import "@/assets/scss/_variables.scss";
+        `,
+      },
+    },
+  }, */
+}
