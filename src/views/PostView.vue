1e031d422ffd9d7f19a34b856f437de1f64e1341
vol_dev <voladores@inbox.ru>
2021-12-15 00:29:05 +0300

    add post-view-actions

--- src/views/PostView.vue
+++ src/views/PostView.vue
@@ -2,6 +2,17 @@
   <div v-if="!loading" class="post-view">
     <div class="container row-gap-2">
       <div class="row">
+        <div v-if="admin" class="post-view-actions">
+          <div
+            class="blog-card-icon pointer"
+            @click.prevent="$router.push({ name: 'PostEdit', params: { id: blog.id }})"
+          >
+            <Icon type="applicationEditOutline" light />
+          </div>
+          <div class="blog-card-icon pointer" @click.prevent="$store.dispatch('blogDeletePost', blog.id)">
+            <Icon type="deleteOutline" light />
+          </div>
+        </div>
         <h2 v-if="blog.title" class="post-view-title" v-text="blog.title" />
         <div v-if="blog.date" class="post-view-date">
           <small class="date">
@@ -17,12 +28,16 @@
 </template>
 
 <script>
-import { createFormatDateTime } from '@/utils/formatters'
-
 import { mapState } from 'vuex'
+import { createFormatDateTime } from '@/utils/formatters'
+import Icon from '@/components/common/Icon.vue'
+
 
 export default {
   name: 'PostView',
+  components: {
+    Icon,
+  },
   data: () => ({
     createFormatDateTime,
   }),
@@ -30,6 +45,7 @@ export default {
     ...mapState({
       blog: (state) => state.blog,
       loading: (state) => state.loading,
+      admin: (state) => state.profile.admin,
     }),
   },
   async created() {
