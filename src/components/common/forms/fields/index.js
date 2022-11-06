7ba8c8a71874364b3598ca74e5b5ccd7f9e7e187
vol_dev <voladores@inbox.ru>
2022-11-06 04:17:32 +0300

    ref re

--- src/components/common/forms/fields/index.js
+++ src/components/common/forms/fields/index.js
@@ -1,12 +1,13 @@
-const PWD_RANGE_MAP = {
+const PWD_MAP = {
   MIN: 6,
   MAX: 20,
+  SPC: '@$!%*#?&',
 }
 
 const RE_MAP = {
   ONE_WORD: /[A-zА-яЁё]{2,}/,
   EMAIL: /^[\w\-\\.]+@([\w-]+\.)+[\w-]{2,4}$/,
-  PASSWORD: new RegExp(`^(?=._[A-Za-z])(?=._\\d)[A-Za-z\\d]{${PWD_RANGE_MAP.MIN},${PWD_RANGE_MAP.MAX}}$`),
+  PASSWORD: new RegExp(`^(?=.*[A-Za-z])(?=.*\\d)(?=.*[${PWD_MAP.SPC}])[A-Za-z\\d${PWD_MAP.SPC}]{${PWD_MAP.MIN},${PWD_MAP.MAX}}$`),
 }
 
 export default {
@@ -23,7 +24,7 @@ export default {
     title: `must contains one digit from 0-9,
 must contains one lowercase characters,
 must contains one uppercase characters,
-must contains one special symbols in the list @#$%,
-length at least ${PWD_RANGE_MAP.MIN} characters and maximum of ${PWD_RANGE_MAP.MAX}`,
+must contains one special symbols in the list ${PWD_MAP.SPC},
+length at least ${PWD_MAP.MIN} characters and maximum of ${PWD_MAP.MAX}`,
   },
 }
